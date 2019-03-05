4f0e8d181a275be883b85add76c9d7d468f5c6fe
akalankadewmith@gmail.com <akalankadewmith@gmail.com>
2019-03-05 23:45:11 +0530

    added forgot pw layout by Dewmith

---
+++ app/src/main/java/com/tecOps/workflow/forgot_pw.java
@@ -0,0 +1,34 @@
+package com.tecOps.workflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import com.kalpa.workflow.R;
+
+
+public class forgot_pw extends AppCompatActivity {
+    private static Button b_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pw);
+        onClickButtonListener();
+    }
+
+    public void onClickButtonListener(){
+        b_button = (Button) findViewById(R.id.button4);
+        b_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        onBackPressed();
+                    }
+                }
+        );
+    }
+
+
+
+}
