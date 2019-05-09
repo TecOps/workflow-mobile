13a39a6f6ebdf6fcb479eba1fa27c8cfae2e77d0
Madura De Silva <Madura.D@CreativeSoftware.com>
2019-05-09 22:00:15 +0530

    EventHistoryView ListView created

---
+++ app/src/main/java/com/tecOps/workflow/view/adapter/EventHistoryAdapter.java
@@ -0,0 +1,68 @@
+package com.tecOps.workflow.view.adapter;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.tecOps.workflow.R;
+import com.tecOps.workflow.databinding.EventhistoryItemBinding;
+import com.tecOps.workflow.view.EventDetails;
+import com.tecOps.workflow.viewModel.EventHistoryViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventHistoryAdapter extends RecyclerView.Adapter<EventHistoryAdapter.MyViewHolder> {
+
+    Context context;
+    List<EventHistoryViewModel> events;
+    public LayoutInflater inflater;
+
+    public EventHistoryAdapter(Context context, List<EventHistoryViewModel> events) {
+        this.context = context;
+        this.events = events;
+    }
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.eventhistory_item, parent, false);
+//        MyViewHolder viewHolder = new MyViewHolder(view);
+//        return viewHolder;
+        if(inflater == null){
+            inflater = LayoutInflater.from(parent.getContext());
+        }
+        EventhistoryItemBinding eventhistoryItemBinding = EventhistoryItemBinding.inflate(inflater, parent,false);
+        return new MyViewHolder(eventhistoryItemBinding);
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        holder.name.setText(events.get(position).toString());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              Toast.makeText(context, events.get(position).toString(), Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(view.getContext(), EventDetails.class);
+                context.startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView body;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.cardViewTitle);
+            body = itemView.findViewById(R.id.cardViewTitle);
+        }
+    }
+}
--- app/src/main/java/com/tecOps/workflow/view/fragments/EventHistoryFragment.java
+++ app/src/main/java/com/tecOps/workflow/view/fragments/EventHistoryFragment.java
@@ -1,19 +1,65 @@
 package com.tecOps.workflow.view.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.tecOps.workflow.R;
+import com.tecOps.workflow.databinding.ActivityEventDetailsBinding;
+import com.tecOps.workflow.model.EventModel;
+import com.tecOps.workflow.repository.EventRepository;
+import com.tecOps.workflow.view.adapter.EventHistoryAdapter;
+import com.tecOps.workflow.viewModel.EventDetailsViewModel;
+import com.tecOps.workflow.viewModel.EventHistoryViewModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class EventHistoryFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private EventModel eventModel;
+    private EventHistoryViewModel eventHistoryViewModel;
+    private View rootView;
+    protected static ActivityEventDetailsBinding activityEventDetailsBinding;
+
+
+    ArrayList personNames = new ArrayList<>(Arrays.asList("Person 1", "Person 2", "Person 3", "Person 4", "Person 5", "Person 6", "Person 7","Person 8", "Person 9", "Person 10", "Person 11", "Person 12", "Person 13", "Person 14"));
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_event_history,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        rootView = inflater.inflate(R.layout.fragment_event_history, container, false);
+        InItDataBinding();
+        return rootView;
+    }
+
+
+    private void InItDataBinding()
+    {
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.eventHistoryListView);
+        layoutManager = new LinearLayoutManager(rootView.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        EventHistoryAdapter mAdapter = new EventHistoryAdapter(getContext(), personNames);
+        recyclerView.setAdapter(mAdapter);
+
+        eventModel=new EventModel();
+
+        eventHistoryViewModel=new EventHistoryViewModel(rootView.getContext(),eventModel);
+        activityEventDetailsBinding = DataBindingUtil.setContentView(this, R.layout.fragment_event_history);
+        activityEventDetailsBinding.setEventModel(eventModel);
+        EventRepository eventRepository=new EventRepository(this,eventModel) ;
+        eventRepository.sendPost("1");
+
+
+        activityEventDetailsBinding.executePendingBindings();
     }
 }
